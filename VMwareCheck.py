383d4f9a876bb3cfb65e75e0168897b998c04e7c
dewa55 <vedran.devcic55@gmail.com>
2020-04-30 23:18:33 +0200

    First commit

---
+++ VMwareCheck.py
@@ -0,0 +1,26 @@
+import paramiko
+def sshexecute ( hostname, username, password ):
+    commands = [
+    "pwd",
+    "id",
+    "uname -a",
+    "df -h"
+   ]
+# initialize the SSH client
+    client = paramiko.SSHClient()
+# add to known hosts
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        client.connect(hostname=hostname, username=username, password=password)
+    except:
+        print("[!] Cannot connect to the SSH Server")
+        exit()
+# execute the commands
+    for command in commands:
+        print("="*50, command, "="*50)
+        stdin, stdout, stderr = client.exec_command(command)
+        print(stdout.read().decode())
+        err = stderr.read().decode()
+        if err:
+            print(err)
+sshexecute( hostname = '172.16.140.165', username = 'root', password = 'Combis123$')
